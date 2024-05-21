4807bad632adb2b3f1d045946b6690c81d9595f0
ferizoozoo <farhadzohoorz@gmail.com>
2024-05-21 17:09:56 +0330

    pass stage

--- app/server.go
+++ app/server.go
@@ -53,8 +53,8 @@ func main() {
 				os.Exit(1)
 			}
 
-			if header := strings.Split(string(line), ":"); header[0] == "User-Agent" {
-				userAgent = header[1]
+			if header := strings.SplitN(string(line), ":", 2); strings.TrimSpace(header[0]) == "User-Agent" {
+				userAgent = strings.TrimSpace(header[1])
 			}
 		}
 		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)))
