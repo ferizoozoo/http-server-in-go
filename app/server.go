0591fc1d0e941dbdf413ba8e0c45d31f7f827841
ferizoozoo <farhadzohoorz@gmail.com>
2024-05-25 20:25:46 +0330

    pass stage

--- app/server.go
+++ app/server.go
@@ -7,26 +7,137 @@ import (
 	"io"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
-//TODO: refactoring using the below todos
-//TODO: create a server struct
-//TODO: create a request struct
-//TODO: create a response struct
-//TODO: create a parserequest func (have a headers map type)
-//TODO: create a handler for each route
-//TODO: create option pattern for each handler
+// TODO: refactoring using the below todos
+// TODO: create a handler for each route
+// TODO: create option pattern for each handler
+type Server struct {
+	port    string
+	ip      string
+	rootDir string
+}
 
-var directory string
+func NewServer(port, ip, rootDir string) *Server {
+	return &Server{
+		port,
+		ip,
+		rootDir,
+	}
+}
+
+func (s *Server) Start() (net.Listener, error) {
+	addr := s.ip + ":" + s.port
+	return net.Listen("tcp", addr)
+}
+
+type Request struct {
+	Headers map[string]string
+	Params  map[string]string
+	Routes  []string
+	Body    string
+	Method  string
+	Url     string
+	Version string
+}
+
+func ParseRequest(reader io.Reader) (*Request, error) {
+	request := &Request{}
+	request.Headers = make(map[string]string)
+	bufreader := bufio.NewReader(reader)
+
+	// read request line
+	line, err := bufreader.ReadString('\n')
+	if err != nil {
+		return nil, err
+	}
+
+	line = strings.Trim(line, "\r\n")
+
+	requestLine := strings.Split(line, " ")
+	request.Method = requestLine[0]
+	request.Url = requestLine[1]
+	request.Version = requestLine[2]
+
+	// read routes and params
+	routesAndParams := strings.Split(request.Url, "?")
+	request.Routes = strings.Split(routesAndParams[0], "/")
+
+	if len(routesAndParams) > 1 {
+		params := strings.Split(routesAndParams[1], "&")
+		for _, param := range params {
+			paramParts := strings.Split(param, "=")
+			request.Params[paramParts[0]] = paramParts[1]
+		}
+	}
+
+	// read headers
+	for {
+		line, err := bufreader.ReadString('\n')
+		if err != nil {
+			return nil, err
+		}
+
+		line = strings.Trim(line, "\r\n")
+		if line == "" {
+			break
+		}
+
+		header := strings.SplitN(line, ":", 2)
+		request.Headers[strings.TrimSpace(header[0])] = strings.TrimSpace(header[1])
+	}
+
+	// read body
+	body := &strings.Builder{}
+
+	for {
+		line, err := bufreader.ReadString('\n')
+		if err != nil && err != io.EOF {
+			return nil, err
+		}
+
+		line = strings.Trim(line, "\r\n")
+		body.WriteString(line)
+		if err == io.EOF {
+			break
+		}
+	}
+
+	request.Body = body.String()
+
+	return request, nil
+}
+
+type Response struct {
+	Headers map[string]string
+	Status  string
+	Version string
+	Message string
+	Body    string
+}
+
+func (res *Response) Write(conn net.Conn) error {
+	h := strings.Builder{}
+
+	for key, value := range res.Headers {
+		h.Write([]byte(fmt.Sprintf("%s: %s\r\n", key, value)))
+	}
+
+	_, err := conn.Write([]byte(fmt.Sprintf("%s %s %s\r\n%s\r\n%s", res.Version, res.Status, res.Message, h.String(), res.Body)))
+	return err
+}
+
+var server *Server
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	var directory string
 	flag.StringVar(&directory, "directory", "", "directory to serve static files from")
 	flag.Parse()
-	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	server = NewServer("4221", "0.0.0.0", directory)
+	l, err := server.Start()
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
@@ -46,106 +157,136 @@ func main() {
 }
 
 func handleConnection(conn net.Conn) {
-	reader := bufio.NewReader(conn)
-
-	line, err := reader.ReadString('\r')
+	req, err := ParseRequest(conn)
 	if err != nil {
 		fmt.Println("Error reading from connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	url := strings.Split(line, " ")
-	path := url[1]
-	method := url[0]
+	var resp Response
 
-	if path == "/" {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		return
-	}
-
-	if path == "/user-agent" {
-		var userAgent string
-		for {
-			line, _, err := reader.ReadLine()
-			if err != nil {
-				if err == io.EOF {
-					break
-				}
-
-				fmt.Println("Error reading from connection: ", err.Error())
-				os.Exit(1)
-			}
-
-			if header := strings.SplitN(string(line), ":", 2); strings.TrimSpace(header[0]) == "User-Agent" {
-				userAgent = strings.TrimSpace(header[1])
-				break
-			}
+	switch {
+	case req.Url == "/":
+		resp = Response{
+			Version: "HTTP/1.1",
+			Status:  "200",
+			Message: "OK",
+			Headers: map[string]string{
+				"Content-Type": "text/html",
+			},
+			Body: "Hello World!",
 		}
-		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)))
-	}
+	case req.Url == "/user-agent":
+		resp = Response{
+			Version: "HTTP/1.1",
+			Status:  "200",
+			Message: "OK",
+			Headers: map[string]string{
+				"Content-Type":   "text/plain",
+				"Content-Length": strconv.Itoa(len(req.Headers["User-Agent"])),
+			},
+			Body: req.Headers["User-Agent"],
+		}
+	case strings.Contains(req.Url, "/files"):
+		filename := req.Routes[0]
+		filePath := server.rootDir + "/" + filename
 
-	if strings.Contains(path, "/files") {
-		if method == "GET" {
-			filename := strings.Split(path, "/")[2]
-			filePath := directory + "/" + filename
+		if req.Method == "GET" {
 			if _, err := os.Stat(filePath); err == nil {
 				data, err := os.ReadFile(filePath)
 				if err != nil {
-					conn.Write([]byte(fmt.Sprintf("HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(err.Error()), err.Error())))
-					return
+					resp = Response{
+						Version: "HTTP/1.1",
+						Status:  "404",
+						Message: "Not Found",
+						Headers: map[string]string{
+							"Content-Type": "text/plain",
+						},
+						Body: err.Error(),
+					}
 				}
 
-				conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(data), string(data))))
-				return
-			}
-		} else if method == "POST" {
-			filename := strings.Split(path, "/")[2]
-			filePath := directory + "/" + filename
-
-			body := &strings.Builder{}
-			buffer := make([]byte, 1024)
-			for {
-				n, err := reader.Read(buffer)
-				if err != nil && err != io.EOF {
-					fmt.Println("Error reading body:", err.Error())
-					conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
-					return
-				}
-				if n == 0 {
-					break
-				}
-				body.Write(buffer[:n])
-				if err == io.EOF {
-					break
+				resp = Response{
+					Version: "HTTP/1.1",
+					Status:  "200",
+					Message: "OK",
+					Headers: map[string]string{
+						"Content-Type":   "application/octet-stream",
+						"Content-Length": strconv.Itoa(len(data)),
+					},
 				}
 			}
-
+		} else if req.Method == "POST" {
 			file, err := os.Create(filePath)
 			if err != nil {
-				conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
-				return
-			}
-			defer file.Close()
+				resp = Response{
+					Version: "HTTP/1.1",
+					Status:  "500",
+					Message: "Internal Server Error",
+					Headers: map[string]string{
+						"Content-Type": "text/plain",
+					},
+					Body: err.Error(),
+				}
 
-			_, err = file.WriteString(body.String())
+			}
+
+			_, err = file.Write([]byte(req.Body))
 			if err != nil {
-				conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
-				return
+				resp = Response{
+					Version: "HTTP/1.1",
+					Status:  "500",
+					Message: "Internal Server Error",
+					Headers: map[string]string{
+						"Content-Type": "text/plain",
+					},
+					Body: err.Error(),
+				}
 			}
 
-			// Send a response
-			conn.Write([]byte("HTTP/1.1 201 Created\r\n\r\n"))
+			resp = Response{
+				Version: "HTTP/1.1",
+				Status:  "201",
+				Message: "Created",
+				Headers: map[string]string{
+					"Content-Type": "text/plain",
+				},
+			}
+		}
+	case strings.Contains(req.Url, "/echo"):
+		if len(req.Routes) == 0 {
+			resp = Response{
+				Version: "HTTP/1.1",
+				Status:  "400",
+				Message: "Bad Request",
+				Headers: map[string]string{
+					"Content-Type": "text/plain",
+				},
+				Body: "No route specified",
+			}
+		}
+		resp = Response{
+			Version: "HTTP/1.1",
+			Status:  "200",
+			Message: "OK",
+			Headers: map[string]string{
+				"Content-Type":   "text/plain",
+				"Content-Length": strconv.Itoa(len(req.Routes[0])),
+			},
+			Body: req.Routes[0],
+		}
+	default:
+		resp = Response{
+			Version: "HTTP/1.1",
+			Status:  "404",
+			Message: "Not Found",
+			Headers: map[string]string{
+				"Content-Type": "text/plain",
+			},
+			Body: "Not Found",
 		}
 
-		conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
-		return
 	}
 
-	if strings.Contains(path, "/echo") {
-		param := strings.Split(path, "/")[2]
-		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(param), param)))
-		return
-	}
-
-	conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	resp.Write(conn)
 }
