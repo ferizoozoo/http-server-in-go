903ddb44026b38a89179be4eed5e912a7bec5f5f
ferizoozoo <farhadzohoorz@gmail.com>
2024-05-25 11:30:29 +0330

    pass stage

--- app/server.go
+++ app/server.go
@@ -15,7 +15,8 @@ var directory string
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	flag.StringVar(&directory, "directory", "app", "directory to serve static files from")
+	flag.StringVar(&directory, "directory", "", "directory to serve static files from")
+	flag.Parse()
 	fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
