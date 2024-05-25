2a3ab4da54af2cf6bcacf41ae8e454fef62ee989
ferizoozoo <farhadzohoorz@gmail.com>
2024-05-25 14:36:14 +0330

    pass stage

--- app/server.go
+++ app/server.go
@@ -10,6 +10,14 @@ import (
 	"strings"
 )
 
+//TODO: refactoring using the below todos
+//TODO: create a server struct
+//TODO: create a request struct
+//TODO: create a response struct
+//TODO: create a parserequest func (have a headers map type)
+//TODO: create a handler for each route
+//TODO: create option pattern for each handler
+
 var directory string
 
 func main() {
@@ -46,14 +54,16 @@ func handleConnection(conn net.Conn) {
 		os.Exit(1)
 	}
 
-	url := strings.Split(line, " ")[1]
+	url := strings.Split(line, " ")
+	path := url[1]
+	method := url[0]
 
-	if url == "/" {
+	if path == "/" {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 		return
 	}
 
-	if url == "/user-agent" {
+	if path == "/user-agent" {
 		var userAgent string
 		for {
 			line, _, err := reader.ReadLine()
@@ -74,23 +84,66 @@ func handleConnection(conn net.Conn) {
 		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)))
 	}
 
-	if strings.Contains(url, "/files") {
-		filename := strings.Split(url, "/")[2]
-		path := directory + "/" + filename
-		if _, err := os.Stat(path); err == nil {
-			data, err := os.ReadFile(path)
+	if strings.Contains(path, "/files") {
+		if method == "GET" {
+			filename := strings.Split(path, "/")[2]
+			filePath := directory + "/" + filename
+			if _, err := os.Stat(filePath); err == nil {
+				data, err := os.ReadFile(filePath)
+				if err != nil {
+					conn.Write([]byte(fmt.Sprintf("HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(err.Error()), err.Error())))
+					return
+				}
+
+				conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(data), string(data))))
+				return
+			}
+		} else if method == "POST" {
+			filename := strings.Split(path, "/")[2]
+			filePath := directory + "/" + filename
+			var body []byte
+			crlfCount := 0
+			for {
+				line, _, err := reader.ReadLine()
+				if err != nil {
+					if err == io.EOF {
+						break
+					}
+
+					fmt.Println("Error reading from connection: ", err.Error())
+					os.Exit(1)
+				}
+
+				if strings.Contains(string(line), "\r\n") {
+					crlfCount++
+				}
+
+				if crlfCount == 2 {
+					body = append(body, line...)
+					break
+				}
+			}
+
+			file, err := os.Create(filePath)
 			if err != nil {
-				conn.Write([]byte(fmt.Sprintf("HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(err.Error()), err.Error())))
+				conn.Write([]byte("HTTP/1.1 400 OK\r\n\r\n"))
+				return
+			}
+			defer file.Close()
+
+			// Write data to the file
+			_, err = file.Write(body)
+			if err != nil {
+				conn.Write([]byte("HTTP/1.1 400 OK\r\n\r\n"))
 				return
 			}
 
-			conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(data), string(data))))
-			return
+			conn.Write([]byte("HTTP/1.1 201 OK\r\n\r\n"))
 		}
 	}
 
-	if strings.Contains(url, "/echo") {
-		param := strings.Split(url, "/")[2]
+	if strings.Contains(path, "/echo") {
+		param := strings.Split(path, "/")[2]
 		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(param), param)))
 		return
 	}
